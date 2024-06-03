7f0dbffdf01c3b9fa2bf9e2248f6ec5e6c8a98f1
ahsan-19 <112505556+ahsan-19@users.noreply.github.com>
2024-06-03 12:36:11 +0500

    improved project pagw

--- frontend/src/pages/home/ProjectPageLayout.jsx
+++ frontend/src/pages/home/ProjectPageLayout.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ProjectsCreation from './ProjectsCreation'
 import PageLeftSide from './PageLeftSide'
 import PageRightSide from './PageRightSide'
+import { getAuthToken } from '../../utility/JWTtokenExport'
+import Login from '../Login'
+import { useNavigate } from 'react-router-dom'
 
 
 const ProjectPageLayout = () => {
+const token = getAuthToken()
+const navigate = useNavigate()
+
+  if (!token) {
+    return (<>
+     {navigate('/login')}
+      <Login />
+    </>
+
+    )
+  }
   return (
     <>
         <div className="flex m-5 p-5">
